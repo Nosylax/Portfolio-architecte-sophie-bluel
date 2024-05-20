b3e65fac25e881ee07866b548af2bc9905427256
Alyson Idaia <alaville@idaia.group>
2024-05-20 22:07:10 +0200

    Avancer page de connexion requête

--- FrontEnd/Login.js
+++ FrontEnd/Login.js
@@ -1,4 +1,35 @@
-export function login() {
-  const login_forms = document.querySelector("login_forms");
-  login_forms.addEventListener("submit", function (event) {});
+function login() {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      outputElement.textContent = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
+
+const login_forms = document.querySelector("submit");
+login_forms.addEventListener("click", function (event) {
+  event.preventDefault();
+  login();
+});
