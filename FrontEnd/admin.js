bdc80157387e3fb1d22eeaf481afe62fcf081ddf
Nosylax <alyson.laville@gmail.com>
2024-05-30 15:24:10 +0200

    Gérer la deconnexion et ajout bouton admin

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -18,4 +18,52 @@ function display_admin_element() {
     }
   });
 }
+
 display_admin_element();
+
+function hide_login() {
+  const log_element = document.querySelector(".login");
+  if (is_connected()) {
+    log_element.classList.add("hidden");
+  } else {
+    log_element.classList.remove("hidden");
+  }
+}
+
+hide_login();
+
+function hide_logout() {
+  const logout_element = document.querySelector(".logout");
+  if (is_connected()) {
+    logout_element.classList.remove("hidden");
+  } else {
+    logout_element.classList.add("hidden");
+  }
+}
+
+hide_logout();
+
+function hide_filter() {
+  const log_element = document.querySelector(".filter");
+  if (is_connected()) {
+    log_element.classList.add("hidden");
+  }
+}
+
+hide_filter();
+
+function hide_custom() {
+  const custom_element = document.querySelector(".custom");
+  if (is_connected()) {
+    custom_element.classList.remove("hidden");
+  } else {
+    custom_element.classList.add("hidden");
+  }
+}
+
+hide_custom();
+
+const remove_token = document.querySelector(".logout");
+remove_token.addEventListener("click", function () {
+  window.localStorage.removeItem("token");
+});
